d98774b65f3b817d35724ac8600cb06cc6595b0e
root <root@ip-172-31-1-207.localdomain>
2013-07-04 18:12:45 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var index=fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  response.end(index);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
